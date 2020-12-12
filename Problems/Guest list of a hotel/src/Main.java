b839577505d5796c46edae382a71be5e582d7fbc
BonDyka <bondarew2507@gmail.com>
2020-12-12 18:47:38 +0300

    first commit

---
+++ Problems/Guest list of a hotel/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println('z' + 0);
+        System.out.println(Character.digit('z',0));
+        System.out.println(Character.getNumericValue((char) ('1' + '3')));
+int a = 'z';
+        System.out.println(a);
+//        while (scn.hasNextLine()) {
+//            while (scn.hasNext()) {
+//                builder.append(scn.next()).append(" ");
+//            }
+//        }
+//        String[] guests = builder.toString().split(" ");
+//        for (int i = guests.length - 1; i >= 0; i--) {
+//            System.out.println(guests[i]);
+//        }
+    }
+}
